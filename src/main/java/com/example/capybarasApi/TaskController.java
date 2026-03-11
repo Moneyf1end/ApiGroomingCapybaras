807187213782c79bbfec3dd719c2e6308ccff1c6
Artimon4ik <gherganovtioma2005@gmail.com>
2026-03-11 22:00:51 +0200

    Added GET endpoints for TypeOfService

--- src/main/java/com/example/capybarasApi/TaskController.java
+++ src/main/java/com/example/capybarasApi/TaskController.java
@@ -1,10 +1,10 @@
 package com.example.capybarasApi;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/service")
@@ -12,4 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Validated
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping("/{id}")
+    public TypeOfService getServiceById(@PathVariable @Min(0) Long id) {
+        log.info("Getting service with id: {}", id);
+        return taskService.getServiceById(id);
+    }
+
+    @GetMapping
+    public Iterable<TypeOfService> getAllServices() {
+        log.info("Getting all services");
+        return taskService.getAllServices();
+    }
+
+
 }
